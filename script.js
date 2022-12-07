bb22b0adbcefc7e6c5a67d45eb992aedaac8d304
Dilnura [FE16] <102959751+Dilnura-d@users.noreply.github.com>
2022-12-07 14:27:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+function setInfo()
+ {let student1 = {
+    name: prompt('Type your name'),
+    surname : prompt('Type your surname'),
+    age : prompt('Type your age'),
+    object: {student1:""
+    }}
+    console.log(student1);
+
+    let student2 = {
+    name: prompt('Type your name'),
+    surname : prompt('Type your surname'),
+    age : prompt('Type your age'),
+    object: {student1:""
+    }}
+    console.log(student2);
+
+    let student3 = {
+    name: prompt('Type your name'),
+    surname : prompt('Type your surname'),
+    age : prompt('Type your age'),
+    object: {student1:""
+    }}
+    console.log(student3);
+
+    let student4 = {
+    name: prompt('Type your name'),
+    surname : prompt('Type your surname'),
+    age : prompt('Type your age'),
+    object: {student1:""
+    }}
+    console.log(student4);
+
+
+    let student5 = {
+        name: prompt('Type your name'),
+        surname : prompt('Type your surname'),
+        age : prompt('Type your age'),
+        object: {student1:""
+            }}
+            console.log(student5);
+}
+setInfo()
